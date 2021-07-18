17a0632bc307c2800c595922a69c66f48b3b93b4
María Muñoz <mabelen_76@hotmail.com>
2021-07-18 14:57:28 +0200

    java

---
+++ scripts.js
@@ -0,0 +1,31 @@
+let primeraCadena = "primeraCadena js";
+console.log(typeof primeraCadena)
+
+let primerNum = 10;
+console.log(typeof primerNum);
+
+let primerBoolean = true;
+console.log(typeof primerBoolean);
+
+let variable;
+console.log(typeof variable);
+
+let belen= [1,2,3,4];
+console.log(belen instanceof Array);
+
+let a = " Buenos días Mi nombre es nicolas sparro"
+let b = "Y me gusta Javascript"
+let string = "Hey Tu, como te llamas. ¡Hey estoy hablando contigo!"
+let result= a.length + b.length;
+
+console.log(a +" "+ b);
+console.log(`${a} ${b}`)
+console.log(a.length+b.length);
+console.log(a + " " + b + " "+ "y la suma de los caracteres de este es"+" "+ result);
+
+console.log(10+"1");
+console.log(typeof (10+"1"));
+console.log(10-"1");
+console.log(typeof (10-"1"));
+console.log(string);
+console.log(string.replace("contigo", "el"));
